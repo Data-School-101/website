64523ec39967418c198b9db3609e73624e5a9d05
Rohianon <rohianon48@gmail.com>
2024-08-20 19:02:49 +0300

    mobile responsiveness

--- components/home/intro-card.tsx
+++ components/home/intro-card.tsx
@@ -1,37 +1,37 @@
 'use client'
-import React from 'react';
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import Section from '../section';
-import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import React from 'react'
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import Section from '../section'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 const defaultProps = {
   imageUrl: 'https://assets.api.uizard.io/api/cdn/stream/f3f59566-013a-4b2b-a359-188b24a4841c.png',
-};
+}
 
 export const IntroSection: React.FC = () => {
   const [text] = useTypewriter({
     words: ['THE DATA SCHOOL PLATFORM', 'YOUR GATEWAY TO ADVANCED ANALYTICS', 'LEARN FROM INDUSTRY EXPERTS'],
     loop: 0, // Infinite loop
     delaySpeed: 6000,
-  });
+  })
 
   return (
     <Section>
-      <div className="relative h-screen w-full flex items-center justify-center ">
-        <div className="relative z-20 w-[28rem]">
-          <Card className="bg-gradient-to-r from-teal-400 via-blue-500 to-purple-600 transparent p-8 rounded-lg shadow-lg">
+      <div className="relative min-h-screen w-full flex flex-col lg:flex-row items-center justify-center">
+        <div className="relative z-20 w-full lg:w-[28rem] px-4 lg:px-0">
+          <Card className="bg-gradient-to-r from-teal-400 via-blue-500 to-purple-600 p-8 rounded-lg shadow-lg">
             <CardHeader>
-              <CardTitle className="text-4xl font-bold text-center text-white">
+              <CardTitle className="text-2xl lg:text-4xl font-bold text-center text-white">
                 <span>{text}</span>
                 <Cursor cursorColor="#FA0F00" />
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <p className="mt-4 text-center text-gray-800">
+              <p className="mt-4 text-center text-white text-sm lg:text-base">
                 Event Schedule<br />
                 MON - FRI, 9 AM - 5 PM<br />
-                Today&apos;s Events<br />
+                Today&aposs Events<br />
                 9 AM - 5 PM
               </p>
             </CardContent>
@@ -40,16 +40,15 @@ export const IntroSection: React.FC = () => {
             </CardFooter>
           </Card>
         </div>
-        <div className="relative transparent right-14 transform w-[35rem] h-[35rem] z-0">
+        <div className="relative mt-8 lg:mt-0 lg:right-14 transform w-full h-[20rem] lg:w-[35rem] lg:h-[35rem] z-0">
           <Card
             className="w-full h-full bg-center bg-cover bg-no-repeat shadow-xl rounded-lg"
             style={{ backgroundImage: `url(${defaultProps.imageUrl})` }}
           >
-            <div className="absolute inset-0 transparent opacity-20 rounded-lg"></div>
+            <div className="absolute inset-0 bg-black opacity-20 rounded-lg"></div>
           </Card>
         </div>
       </div>
     </Section>
-  );
-};
-
+  )
+}
