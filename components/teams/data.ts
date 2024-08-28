624fd4f9af892fab1412b2412729b421a1374864
Henry Athiany <henry.athiany@gmail.com>
2024-08-29 00:04:32 +0300

    Update data.ts
    updated team member details

--- components/teams/data.ts
+++ components/teams/data.ts
@@ -8,57 +8,48 @@ interface TeamMember {
 
 export const teamMembers: TeamMember[] = [
   {
-    name: "Dr. Emily Turner",
-    title: "Senior Lecturer",
+    name: "Caroline Mugo",
+    title: "Team Lead and GIS Lead",
     description:
-      "Dr. Turner specializes in statistical modeling and data analytics. She has over 15 years of experience in the field and is passionate about helping students understand complex concepts.",
+      "Caroline is the current Team Lead of the Datacraft Lab",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/b0f7bf37-bb14-43e9-a870-e643650e7336.png", // Updated image URL
     linkedinUrl: "https://www.linkedin.com/in/emily-turner",
   },
   {
-    name: "Prof. Michael Johnson",
-    title: "Head of Department",
+    name: "Henry Athiany",
+    title: "Project Coordinator and Biostatistics Lead",
     description:
-      "Prof. Johnson leads the statistics department with a focus on innovative teaching methods. He has published numerous papers on statistical theory and education.",
+      "Project Coordinator and Biostatistics Lead",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/bd2ebabb-8cb1-4086-b714-5ad9c06d508b.png", // Updated image URL
-    linkedinUrl: "https://www.linkedin.com/in/michael-johnson",
+    linkedinUrl: "https://www.linkedin.com/in/henry-athiany-a755651b/",
   },
   {
-    name: "Dr. Sarah Lee",
-    title: "Lecturer",
+    name: "Winnie Mbusiro Chacha",
+    title: "Financial Modelling and Training Lead",
     description:
-      "Dr. Lee's research interests include biostatistics and data visualization. She is dedicated to making statistics accessible and engaging for all students.",
+      "Financial Modelling and Training Lead",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/903aac7c-f4c4-4e0a-97ac-2bf1fa9ca905.png", // Updated image URL
-    linkedinUrl: "https://www.linkedin.com/in/sarah-lee",
+    linkedinUrl: "https://www.linkedin.com/in/dr-winnie-chacha-94946764/",
   },
   {
-    name: "Ms. Laura Brown",
-    title: "Teaching Assistant",
+    name: "Charity Mkajuma",
+    title: "Actuarial, Operations Research and Research Lead",
     description:
-      "Ms. Brown focuses on computational statistics and data science applications. She is committed to supporting students in their academic and professional journeys.",
+      "Charity is teh current Actuarial Sciecne  and Operations research lead and Head of Research at DataCraft Lab",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/868bb938-92c4-4491-9e66-c2205214c3eb.png",
-    linkedinUrl: "https://www.linkedin.com/in/laura-brown",
+    linkedinUrl: "https://www.linkedin.com/in/charity-wamwea-5122a471/",
   },
   {
-    name: "Dr. John Smith",
-    title: "Assistant Lecturer",
+    name: "Joseph Mungatu",
+    title: "Statistics and Collaborations Lead",
     description:
-      "Dr. Smith is an expert in probability theory and has a knack for simplifying complex topics. He enjoys mentoring students and fostering a collaborative learning environment.",
+      "Statistics and Collaborations Lead",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/62524e83-d97b-4b36-95ab-8446e168a71f.png",
-    linkedinUrl: "https://www.linkedin.com/in/john-smith",
-  },
-  {
-    name: "Dr. Alan White",
-    title: "Researcher",
-    description:
-      "Dr. White’s research involves advanced statistical techniques and their applications in real-world problems. He is passionate about bridging the gap between theory and practice.",
-    imageSrc:
-      "https://assets.api.uizard.io/api/cdn/stream/d695807e-de07-4d8a-b90f-e4f34cf7ce38.png",
-    linkedinUrl: "https://www.linkedin.com/in/alan-white",
+    linkedinUrl: "https://www.linkedin.com/in/joseph-kyalo-mung-atu-98957570/",
   },
 ]
