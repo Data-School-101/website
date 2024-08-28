ae439c207f2fe0e29bbb8291d15d0faa23d3e1c5
Henry Athiany <henry.athiany@gmail.com>
2024-08-29 00:26:49 +0300

    Update event-card.tsx
    update

--- components/home/event-card.tsx
+++ components/home/event-card.tsx
@@ -6,7 +6,7 @@ import Section from '../section'
 export const Events: React.FC = () => {
   return (
     <Section className="py-10 px-6 bg-white">
-      <h2 className="text-center text-3xl font-bold mb-8">This week&apos;s events</h2>
+      <h2 className="text-center text-3xl font-bold mb-8">Coming up...</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <EventCard
           image="https://images.unsplash.com/photo-1507208773393-40d9fc670acf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDUzMDJ8MHwxfHNlYXJjaHw1fHxOdW1iZXJzJTJDJTIwQ2xhc3Nyb29tJTJDJTIwTGFwdG9wfGVufDF8fHx8MTcyMzc5MDgxMHww&ixlib=rb-4.0.3&q=80&w=1080"
@@ -15,12 +15,12 @@ export const Events: React.FC = () => {
         />
         <EventCard
           image="https://images.unsplash.com/photo-1556155092-490a1ba16284?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDUzMDJ8MHwxfHNlYXJjaHw2fHxEYXRhJTJDJTIwUmVzZWFyY2glMkMlMjBMYXB0b3B8ZW58MXx8fHwxNzIzNzkwODExfDA&ixlib=rb-4.0.3&q=80&w=1080"
-          title="Jennifer Smith: Statistical Insights"
+          title="Statistical Insights"
           description="A series of expert talks on statistical insights and their practical applications."
         />
         <EventCard
           image="https://images.unsplash.com/photo-1547032175-7fc8c7bd15b3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDUzMDJ8MHwxfHNlYXJjaHwzfHxBbmFseXRpY3N8ZW58MXx8fHwxNzIzNzg0NTk3fDA&ixlib=rb-4.0.3&q=80&w=1080"
-          title="Michael Johnson: Data Visualization"
+          title="Data Visualization"
           description="Join us for a session on data visualization techniques and tools. Learn how to effectively communicate statistical findings through compelling visual representations."
         />
       </div>
