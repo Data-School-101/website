b8aaee6709d5b2771b428af1c317cea328dbfe5b
Henry Athiany <henry.athiany@gmail.com>
2024-08-29 00:13:23 +0300

    Update data.ts
    update

--- components/teams/data.ts
+++ components/teams/data.ts
@@ -11,7 +11,7 @@ export const teamMembers: TeamMember[] = [
     name: "Caroline Mugo",
     title: "Team Lead and GIS Lead",
     description:
-      "Caroline is the current Team Lead of the Datacraft Lab",
+      "DataCraft Lab Team Lead",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/b0f7bf37-bb14-43e9-a870-e643650e7336.png", // Updated image URL
     linkedinUrl: "https://www.linkedin.com/in/emily-turner",
@@ -20,7 +20,7 @@ export const teamMembers: TeamMember[] = [
     name: "Henry Athiany",
     title: "Project Coordinator and Biostatistics Lead",
     description:
-      "Project Coordinator and Biostatistics Lead",
+      "DataCraft Lab Project Coordinator",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/bd2ebabb-8cb1-4086-b714-5ad9c06d508b.png", // Updated image URL
     linkedinUrl: "https://www.linkedin.com/in/henry-athiany-a755651b/",
@@ -29,7 +29,7 @@ export const teamMembers: TeamMember[] = [
     name: "Winnie Mbusiro Chacha",
     title: "Financial Modelling and Training Lead",
     description:
-      "Financial Modelling and Training Lead",
+      "DataCraft Lab Training Lead",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/903aac7c-f4c4-4e0a-97ac-2bf1fa9ca905.png", // Updated image URL
     linkedinUrl: "https://www.linkedin.com/in/dr-winnie-chacha-94946764/",
@@ -38,7 +38,7 @@ export const teamMembers: TeamMember[] = [
     name: "Charity Mkajuma",
     title: "Actuarial, Operations Research and Research Lead",
     description:
-      "Charity is teh current Actuarial Sciecne  and Operations research lead and Head of Research at DataCraft Lab",
+      "DataCraft Lab Research Lead",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/868bb938-92c4-4491-9e66-c2205214c3eb.png",
     linkedinUrl: "https://www.linkedin.com/in/charity-wamwea-5122a471/",
@@ -47,7 +47,7 @@ export const teamMembers: TeamMember[] = [
     name: "Joseph Mungatu",
     title: "Statistics and Collaborations Lead",
     description:
-      "Statistics and Collaborations Lead",
+      "DataCraft Lab Collaborations Lead",
     imageSrc:
       "https://assets.api.uizard.io/api/cdn/stream/62524e83-d97b-4b36-95ab-8446e168a71f.png",
     linkedinUrl: "https://www.linkedin.com/in/joseph-kyalo-mung-atu-98957570/",
